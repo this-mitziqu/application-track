b2acd6959dd236275f745d4671d80cc0280789ef
izaktan <xiaotan828@gmail.com>
2021-04-16 20:35:55 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import ReactDOM from 'react-dom'
+import React from 'react'
 import './App.css';
+import axios from 'axios';
+import InputForm from './InputForm'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <InputForm />
 }
 
+
 export default App;
+
