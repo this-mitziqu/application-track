549d888bbd693131e5b865329547fc77f958f3b9
miqu <miqu@umich.edu>
2021-04-17 01:27:22 +0800

    fetch api dynamicly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import axios from 'axios';
+
 import InputForm from './InputForm'
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
--- src/InputForm.js
+++ src/InputForm.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 import { Form, InputGroup, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+
+var id = 437133
 
 class InputForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-
+    this.state = {value: 437133};
+    this.state = {posts: []};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -17,12 +20,32 @@ class InputForm extends React.Component {
   }
 
   handleSubmit(event) {
-    alert('输入的ID为: ' + this.state.value);
+  
+    axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + this.state.value, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+      },
+      }).then((response) => this.setState({posts: response.data.title})
+      ).catch(function (error) {
+        if (error.response) {
+          console.log(error.response.headers);
+        } 
+        else if (error.request) {
+            console.log(error.request);
+        } 
+        else {
+          console.log(error.message);
+        }
+      console.log(error.config);
+    });
     event.preventDefault();
   }
 
   render() {
+    const { posts } = this.state
     return (
+      <div>
     <Form onSubmit={this.handleSubmit}>
     <InputGroup className="mb-3">
     <Form.Control
@@ -30,7 +53,7 @@ class InputForm extends React.Component {
       aria-label="请输入ID"
       aria-describedby="basic-addon2"
       type="text" 
-      value={this.state.value} 
+      value={this.state.value || ''} 
       onChange={this.handleChange}
     />
     <InputGroup.Append>
@@ -38,6 +61,8 @@ class InputForm extends React.Component {
     </InputGroup.Append>
     </InputGroup>
   </Form>
+  <div>{posts}</div>
+  </div>
     );
   }
 }
