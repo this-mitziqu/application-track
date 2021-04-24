b1aa5a7da7f500fd14b6bb802bfa4954c29cb71a
izaktan <xiaotan828@gmail.com>
2021-04-24 20:54:14 +0800

    improve layout

--- src/ProgressCard.js
+++ src/ProgressCard.js
@@ -5,10 +5,10 @@ import ProgressBar from './ProgressBar';
 
 class ProgressCard extends React.Component {
     dispatchCard() {
-        console.log(this.props.app)
+        // console.log(this.props.app)
         return JSON.parse(this.props.app.trackInfo).map((branch, i) => {
             return(
-                <Card className="py-4 my-5">
+                <Card className="py-4 mt-0 mb-5" key={i}>
                     <Row className="d-flex justify-content-between px-3 pt-4 pb-5 top">
                         <div className="d-flex flex-column font-weight-bold">
                             <p className="mb-0">所属申请：<span>{this.props.app.appName+' - '+branch.branchName}</span></p>
@@ -27,9 +27,11 @@ class ProgressCard extends React.Component {
     
     render() {
         return(
-            <Container className="px-1 my-5 px-md-4 mx-auto">
+            <Container className="px-1 mb-5 px-md-4 mx-auto">
                 {this.dispatchCard()}
-                <Button variant="primary" onClick={()=>this.props.onClick()}>返回列表</Button>
+                <div className="text-center">
+                    <Button variant="primary" onClick={()=>this.props.onClick()}>返回列表</Button>
+                </div>
             </Container>
         );
     }
